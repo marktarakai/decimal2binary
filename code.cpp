08e6a22dd61c982c33f1045af93f7f83a62386f9
Mark Tarakai <zmarak.tarakai@gmail.com>
2016-06-26 11:57:57 -0500

    Save my code

---
+++ code.cpp
@@ -0,0 +1,33 @@
+// Decimal to Binary Converter
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  // Var Declarations
+  int numDec = 0;
+  long long numBinary = 0;
+
+  // User Prompt
+  cout << "Enter a decimal number to be converted to binary: ";
+  cin >> numDec;
+
+  if(numDec = 0)
+  {
+    cout << numBinary << endl;
+  }
+  else
+  {
+    do {
+
+      for (size_t numCount = 0; numCount < numDec; numCount++) {
+
+        
+
+      }
+    } while(numDec != 0);
+  }
+
+  return 0;
+}
